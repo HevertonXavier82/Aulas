602f1358791bceda8d816d0acd39188e63454a36
HevertonXavier82 <114035076+HevertonXavier82@users.noreply.github.com>
2024-03-04 20:40:40 -0400

    Add files via upload

---
+++ aula05.java
@@ -0,0 +1,16 @@
+package aulas;
+
+public class aula05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        double numero = 49;
+        double numero1 = 4;
+        numero = Math.sqrt(numero);
+        numero1=Math.pow(numero1, 2);
+        
+        System.out.println("Valor da raiz:"+numero);
+        System.out.println("Valor ao quadrado:"+numero1);
+	}
+
+}
